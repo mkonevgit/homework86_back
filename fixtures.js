dacd5731e1c7a5c6e753ccb58bbbd134267b2e6c
Mikhail Konev <mkonev2020@gmail.com>
2021-08-09 14:58:44 +0600

    #1 change packege.json and fixtures.js

--- fixtures.js
+++ fixtures.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
-const User = require("./../models/User");
-const Artist = require("./../models/Artist");
-const Album = require("./../models/Album");
-const Track = require("./../models/Track");
+const User = require("./models/User");
+const Artist = require("./models/Artist");
+const Album = require("./models/Album");
+const Track = require("./models/Track");
 
 
 const {nanoid} = require("nanoid");
 
-mongoose.connect("mongodb://db:27017/musicDB",
+mongoose.connect("mongodb://localhost:27017/musicDB",
    {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true, useCreateIndex: true},
 );
 
